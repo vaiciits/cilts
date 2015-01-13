5d018967ce9fffe2975d82662e242fcc9dbe8af1
vaiciits <vaicekauskis.m@gmail.com>
2015-01-13 17:52:46 +0200

    Main and structure added

---
+++ cilts.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct entry{
+    int pk;
+    char s;
+    string name;
+    string surname;
+    int parent1_pk;
+    int parent2_pk;
+};
+
+int main(){
+
+}
